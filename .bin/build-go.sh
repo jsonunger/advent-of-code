d540951a69cbd073a1a6cc67f806aa8d68e6533c
Jason Unger <jason.unger@policygenius.com>
2021-11-29 15:27:59 -0500

    Introduce initial golang code

---
+++ .bin/build-go.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Build directory for running go code
+
+cwd=$(pwd)
+year=$(date +'%Y')
+day=$(date +'%-d')
+
+while getopts d:y: flag
+do
+    case "${flag}" in
+        y) year=${OPTARG};;
+        d) day=${OPTARG};;
+    esac
+done
+
+day_dir="$year/day$day"
+
+echo -e "Checking for $day_dir..."
+
+if [[ -d "$day_dir" ]]; then
+    echo "$day_dir already exists"
+    exit 1
+fi
+
+echo "Creating $day_dir directory..."
+
+mkdir -p $day_dir
+
+echo "Adding base files"
+
+cd $day_dir
+
+touch test_case.txt
+touch input.txt
+
+cat >> main.go <<EOL
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Hello world!")
+}
+EOL
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jsonunger/advent-of-code
+
+go 1.17
